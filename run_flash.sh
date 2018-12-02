e198452da322b337e9cbc3f682c31b0178a9d126
Francesco Anselmo <francesco.anselmo@LDNML14RHV2F.local>
2018-12-02 07:24:22 +0000

    started develop branch

--- run_flash.sh
+++ run_flash.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-flash --hostname media-warrior \
-      --userdata mw-user-data.yaml \
-      --bootconf mw_config.txt \
+flash --hostname lushroom \
+      --userdata lr-user-data.yaml \
+      --bootconf lr_config.txt \
       hypriotos-rpi-v1.9.0.img
