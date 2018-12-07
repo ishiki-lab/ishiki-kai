5dc383ddd136adbac37a76c5ac04dc89e58c7fd8
pisuke <pisuke@tiscali.co.uk>
2018-12-07 18:34:16 +0000

    json update fixes

--- docker/lushroom-rclone/update_files_json.py
+++ docker/lushroom-rclone/update_files_json.py
@@ -3,19 +3,24 @@
 import os, commands
 from os.path import abspath, join
 from os import system, chdir, walk
+from shutil import copyfile
 
 MEDIA_DIR = "/media/usb/tracks"
-RCLONE_CMD = "rclone lsjson %s"
+RCLONE_CMD = "rclone lsjson lushrooms:Tracks/%s"
 RCLONE_COPY_CMD = "rclone copy %s lushrooms:Tracks/%s"
 FILENAME = "content.json"
 
-debug = False
+debug = True
 
 def json_folder(target_dir):
-    cmd = RCLONE_CMD % abspath(target_dir)
+    if target_dir == ".":
+        cmd = RCLONE_CMD % ""
+    else:
+        cmd = RCLONE_CMD % target_dir
     print(cmd)
     json = commands.getoutput(cmd)
-    outfilename = join(abspath(target_dir),FILENAME)
+    #outfilename = join(abspath(target_dir),FILENAME)
+    outfilename = FILENAME
     if debug: print(outfilename, json)
     filename = open(outfilename,'w')
     filename.write(json)
@@ -24,15 +29,16 @@ def json_folder(target_dir):
         cmd = RCLONE_COPY_CMD % (outfilename, "")
     else:
         cmd = RCLONE_COPY_CMD % (outfilename, join(target_dir,""))
+        copyfile(outfilename, join(target_dir,FILENAME))
     print(cmd)
     system(cmd)
 
 def main():
     chdir(MEDIA_DIR)
-    json_folder(".")
     for root, dirs, files in walk(".", topdown = False):
         for name in dirs:
             json_folder(name)
+    json_folder(".")
 
 if __name__ == "__main__":
     main()
