57606425bb3ed7dba1db421098e36d9f5daed96e
Francesco Anselmo <francesco.anselmo@gmail.com>
2018-12-20 11:13:32 +0000

    add brightness to lrpi_play for DMX

--- lrpi_play.py
+++ lrpi_play.py
@@ -95,8 +95,8 @@ def trigger_light(subs):
                     bridge.set_light(l, cmd)
             if scope[0:3] == "DMX":
                 l = int(scope[3:])
-                channels = array(items.split(",")).astype(int)
-                channels = map(lambda i: int(MAX_BRIGHTNESS)*i, channels)
+                channels = int(MAX_BRIGHTNESS)*(array(items.split(",")).astype(int))
+                # channels = array(map(lambda i: int(MAX_BRIGHTNESS)*i, channels))
                 if DEBUG:
                     print("Trigger DMX:", l, channels)
                 if PLAY_DMX:
