02fdf5414f0fab38e67d673d9ae2aa26522e5ceb
root <root@lushroom1-pi.localdomain>
2019-01-20 11:07:43 +0000

    startup scripts

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 python /opt/lushroom/wifi.py
 docker pull arupiot/lrpi_base:latest
-docker run --privileged -v /etc/opt/lushroom:/etc/opt/lushroom --rm lushroom/lrpi_base:latest
+#docker run --privileged -v /etc/opt/lushroom:/etc/opt/lushroom --rm lushroom/lushroom-bootstrap:latest
 docker-compose -f /etc/opt/lushroom/docker-compose.yml pull
 docker-compose -f /etc/opt/lushroom/docker-compose.yml up -d --force-recreate
