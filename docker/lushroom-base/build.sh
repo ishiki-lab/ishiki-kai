6ca787fc1a0b4459fbfb0a8daea0bdb44e92fd75
Paul Harter <paul@glowinthedark.co.uk>
2019-01-31 22:14:14 +0000

    Moving all card preperation into Fabile.
    - Switching to using Raspian
    - All configuration in fabfile
    - Service to run bootstrap script on startup and when usb plugged in
    - Modify Docker daemon.json to use localhost port 2375 as well as unix
    - sshd config files to limit remote access to only with rsa key
    - smb config file
    - Removing now unused old config files

--- docker/lushroom-base/build.sh
+++
@@ -1,6 +0,0 @@
-# lushroom-base Dockerfile
-
-sudo docker build -t lushroom-base .
-sudo docker images
-sudo docker tag lushroom-base arupiot/lushroom-base:latest
-sudo docker push arupiot/lushroom-base
--- docker/lushroom-rclone/build.sh
+++
@@ -1,6 +0,0 @@
-# lushroom-rclone Dockerfile
-
-sudo docker build -t lushroom-rclone .
-sudo docker images
-sudo docker tag lushroom-rclone lushdigital/lushroom-rclone:latest
-sudo docker push lushroom/lushroom-rclone
