895712f884001d88f50a50a50c52705477cf7309
InBrewJ <jasonator@ladsladslads.london>
2019-02-18 01:40:05 +0000

    #21 we now only try to part with a PartyPi with the slave_ip in the master settings and the pair button only appears if we find a slave_ip. When we are in party mode, an as yet non-functional 'unpair' button appears

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,9 @@ export class AppComponent implements OnInit {
   title = 'mw_serve';
   settings: any;
   roomName: string = '?';
+  canPair: boolean = false;
+  partyModeActive: boolean = false;
+  slaveIp: string
 
 
   constructor(
@@ -33,18 +36,28 @@ export class AppComponent implements OnInit {
         let status: any = statusRes
         // this.tracksService.setStatus(status);
         // console.log('ac: ', this.tracksService.getInternalStatus())
+        console.log('settings: ', settings);
+        this.slaveIp = settings.slave_ip;
         this.settings = settings;
         // console.log(this.settings);
         this.roomName = settings.roomName
+        // this.slaveIp = settings.
       }
     );
   }
 
   pair() {
-    this.pairingService.pair('liverpool-06.local').subscribe(
+    this.pairingService.pair(this.slaveIp).subscribe(
       (res: any) => {
         console.log('Pair res: ', res);
+        if(res === 0) {
+          this.partyModeActive = true;
+        }
       }
     )
+  }
+
+  unpair() {
+    console.log('Unpair has not yet been written...');
   }
 }
